18e643d4d0f70b9cc9f8579ea0cf7c0db2562fe4
mohamedyusuf787 <mohamedyusuf13m2003@gmail.com>
2026-04-06 13:21:29 +0530

    infinte loop solved

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -6,23 +6,23 @@ import Footer from './common/Footer';
 function Blogs() {
 
     const [blogs, setBlogs] = useState([]);
-    const [admin,setAdmin]=useState(false);
+    const [admin, setAdmin] = useState(false);
 
     // it call only once when page refresh
 
-useEffect(()=>{
-    window.scrollTo(0,0);
-},[])
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
 
     useEffect(() => {
-        auth.onAuthStateChanged((user)=>{
-            if(user){
-                if(user.uid === "04uyCr7i3wcz4FwRs8sYzfKPL0w2"){
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                if (user.uid === "04uyCr7i3wcz4FwRs8sYzfKPL0w2") {
                     setAdmin(true)
                     console.log("you are admin")
                     console.log(blogs)
                 }
-                else{
+                else {
                     setAdmin(false)
                     console.log("Not an admin")
                 }
@@ -35,7 +35,7 @@ useEffect(()=>{
         }).catch(() => {
             console.log("Error fetching data")
         })
-    }, [blogs])
+    }, [])
 
 
     const [newTitle, setNewTitle] = useState('');
@@ -48,13 +48,13 @@ useEffect(()=>{
             // After successfully updating the likes count in the backend, fetch the updated list of blogs
             if (response.status === 200) {
                 axios.get("https://yusuf-blog-portfolio.onrender.com/api/blogs")  //axios return promise so we put .then and .catch
-                .then((res) => {
-                    console.log(res.data)
-                    setBlogs(res.data)
-                })
-                .catch(() => {
-                    console.log("Error fetching data")
-                })
+                    .then((res) => {
+                        console.log(res.data)
+                        setBlogs(res.data)
+                    })
+                    .catch(() => {
+                        console.log("Error fetching data")
+                    })
             }
         } catch (error) {
             console.error('Error liking the blog post:', error);
@@ -68,19 +68,16 @@ useEffect(()=>{
 
 
         const likes = 0
-        axios.post("https://yusuf-blog-portfolio.onrender.com/api/blogs", { newTitle, date, newContent, likes }).then((res) => {
-            console.log(res.data)
-
-            axios.get("https://yusuf-blog-portfolio.onrender.com/api/blogs").then((res) => {
+            axios.post("https://yusuf-blog-portfolio.onrender.com/api/blogs", { newTitle, date, newContent, likes }).then((res) => {
                 console.log(res.data)
-                setBlogs(res.data)
-            }).catch(() => {
-                console.log("Error fetching data")
-            })
 
-        });
-
-
+                axios.get("https://yusuf-blog-portfolio.onrender.com/api/blogs").then((res) => {
+                    console.log(res.data)
+                    setBlogs(res.data)
+                }).catch(() => {
+                    console.log("Error fetching data")
+                })
+            });
 
 
         setNewTitle('');
@@ -90,34 +87,34 @@ useEffect(()=>{
     return (
         <div className="blog-section pt-14">
             <div className="blog-headings flex flex-col gap-3 items-center  mb-14 px-4">
-            <h2 className="text-center text-5xl font-bold text-white ">Latest  <span className='text-orange-400 text-center'>Blogs</span> 📚</h2>
-            <p className='text-gray-400 text-center'>Sharing my learnings, experiments, and insights from building modern web applications.</p>
-</div>
+                <h2 className="text-center text-5xl font-bold text-white ">Latest  <span className='text-orange-400 text-center'>Blogs</span> 📚</h2>
+                <p className='text-gray-400 text-center'>Sharing my learnings, experiments, and insights from building modern web applications.</p>
+            </div>
             {/* Blog creation form */}
-            {admin?
-            <div className="blog-creation-form mb-8" style={{ width: "80%", margin: "auto" }}>
-                <form onSubmit={handleNewBlogSubmit} className="flex flex-col gap-4 items-center w-full">
-                    <input
-                        type="text"
-                        placeholder="Blog Title"
-                        value={newTitle}
-                        onChange={(e) => setNewTitle(e.target.value)}
-                        className="p-2 border rounded  w-full"
-                        required
-                    />
-                    <textarea
-                        placeholder="Blog Content"
-                        value={newContent}
-                        onChange={(e) => setNewContent(e.target.value)}
-                        className="p-2 border rounded  w-full"
-                        rows="4"
-                        required
-                    />
-                    <button type="submit" className="button-style  w-96">
-                        Add Blog
-                    </button>
-                </form>
-            </div>:""}
+            {admin ?
+                <div className="blog-creation-form mb-8" style={{ width: "80%", margin: "auto" }}>
+                    <form onSubmit={handleNewBlogSubmit} className="flex flex-col gap-4 items-center w-full">
+                        <input
+                            type="text"
+                            placeholder="Blog Title"
+                            value={newTitle}
+                            onChange={(e) => setNewTitle(e.target.value)}
+                            className="p-2 border rounded  w-full"
+                            required
+                        />
+                        <textarea
+                            placeholder="Blog Content"
+                            value={newContent}
+                            onChange={(e) => setNewContent(e.target.value)}
+                            className="p-2 border rounded  w-full"
+                            rows="4"
+                            required
+                        />
+                        <button type="submit" className="button-style  w-96">
+                            Add Blog
+                        </button>
+                    </form>
+                </div> : ""}
 
             <div className="blogs-container grid grid-cols-1 md:grid-cols-2 gap-6 container mx-auto my-10 px-4">
                 {blogs.map((blog) => (
@@ -131,7 +128,7 @@ useEffect(()=>{
                 ))}
             </div>
 
-            <Footer/>
+            <Footer />
         </div>
     );
 }
